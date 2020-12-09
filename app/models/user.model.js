4d04158675090280c877355f8c0871fa6606d1c7
Chetan <chetanrau@cybage.com>
2020-12-09 09:03:14 +0530

    authentication

---
+++ app/models/user.model.js
@@ -0,0 +1,28 @@
+module.exports = (sequelize, Sequelize) => {
+  const User = sequelize.define("users", {
+    first_name: {
+      type: Sequelize.STRING
+    },
+    last_name: {
+      type: Sequelize.STRING
+    },
+    username: {
+      type: Sequelize.STRING
+    },
+    email: {
+      type: Sequelize.STRING
+    },
+    password: {
+      type: Sequelize.STRING
+    }
+    // role: {
+    //   type: Sequelize.INTEGER,
+    //   references: {
+    //     model: 'role',
+    //     key : 'id'
+    //   }
+    //}
+  });
+
+  return User;
+};
